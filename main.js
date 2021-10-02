16b317fbdb16f8ba675652084b066fb31c247413
unknown <you@example.com>
2021-10-02 14:27:14 +0300

    2 commit

--- main.js
+++ main.js
@@ -1,38 +1,26 @@
 //A Javascript Program to illustrate Caesar Cipher Technique
-     
-    // Encrypts text using a shift od s
-    const readlineSync = require('readline-sync');
-    
-    function encrypt(text, s)
-    {
-        let result=""
-        
-        
-        for (let i = 0; i < text.length; i++)
-        {
-            let char = text[i];
-             
-            if (char.toLowerCase())
-            {
-                let ch =  String.fromCharCode((char.charCodeAt(0) + s-65) % 26 + 65);
-                result += ch;
-                
-            } 
-             else{
-               let ch =String.fromCharCode((char.charCodeAt(0) + s-97) % 26 + 97);
-              result += ch
-            
-            }
-           
-        }
-        return result;
-     
-        
-        
+
+// Encrypts text using a shift od s
+const readlineSync = require("readline-sync");
+
+function encrypt(text, shift) {
+  let result = "";
+
+  for (let i = 0; i < text.length; i++) {
+    let char = text.charCodeAt(i);
+
+    if (char >= 65 && char <= 90) {
+      result += String.fromCharCode(((char - 65 + shift) % 26) + 65);
+    } else if (char >= 97 && char <= 122) {
+      result += String.fromCharCode(((char - 97 + shift) % 26) + 97);
+    } else {
+      result += text.charAt(i);
     }
-     
-    // Driver code
-    text= readlineSync.question("give number:")
-    s=Number(readlineSync.question("dwse: "));
-    console.log(encrypt(text,s))
-     
+  }
+  return result;
+}
+
+// Driver code
+text = readlineSync.question("give number:");
+shift = Number(readlineSync.question("dwse: "));
+console.log(encrypt(text, shift));
